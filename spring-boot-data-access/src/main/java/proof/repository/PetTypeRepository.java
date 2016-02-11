96ab9e3c748e43b840c3cf29f47a577e70caebae
Cèsar Ordiñana <cordin@disid.com>
2016-02-11 08:20:27 +0100

    Set transactional readonly again to the PetTypeRepository

--- spring-boot-data-access/src/main/java/proof/repository/PetTypeRepository.java
+++ spring-boot-data-access/src/main/java/proof/repository/PetTypeRepository.java
@@ -1,7 +1,10 @@
 package proof.repository;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import proof.model.PetType;
 
+@Transactional(readOnly = true)
 public interface PetTypeRepository extends ReadOnlyRepository<PetType, Long> {
     // Nothing to add
 }
