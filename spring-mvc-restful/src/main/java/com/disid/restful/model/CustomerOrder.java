332e2cf6d1f4470a8ad9698623676adf248cae92
Cèsar Ordiñana <cordin@disid.com>
2016-05-30 10:09:16 +0200

    Change date fields format

--- spring-mvc-restful/src/main/java/com/disid/restful/model/CustomerOrder.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/model/CustomerOrder.java
@@ -1,16 +1,18 @@
 package com.disid.restful.model;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
 import org.springframework.roo.addon.javabean.annotations.RooToString;
 import org.springframework.roo.addon.jpa.annotations.entity.RooJpaEntity;
-import java.util.Date;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import org.springframework.format.annotation.DateTimeFormat;
-import java.util.HashSet;
-import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.OneToMany;
-import javax.persistence.ManyToOne;
 
 @RooJavaBean
 @RooToString
@@ -20,13 +22,13 @@ public class CustomerOrder {
     /**
      */
     @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "M-")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date orderDate;
 
     /**
      */
     @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "M-")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date shippedDate;
 
     /**
