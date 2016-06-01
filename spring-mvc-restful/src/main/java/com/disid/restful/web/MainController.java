03bd929809c8547b2f1e6e68e4aa30188015a69c
Cèsar Ordiñana <cordin@disid.com>
2016-06-01 09:20:42 +0200

    Remove uneeded locale language parameter

--- spring-mvc-restful/src/main/java/com/disid/restful/web/MainController.java
+++ spring-mvc-restful/src/main/java/com/disid/restful/web/MainController.java
@@ -1,6 +1,16 @@
 package com.disid.restful.web;
 import org.springframework.roo.addon.web.mvc.thymeleaf.annotations.RooThymeleafMainController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RooThymeleafMainController
+@Controller
 public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String index(Model model) {
+        return "index";
+    }
 }
